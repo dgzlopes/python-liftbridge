c3d8d0fd7cfa990afebc5dc6bc9abca1d9b6eb5e
dgzlopes <danielgonzalezlopes@gmail.com>
2019-10-09 09:13:20 +0200

    Add tls support
    Signed-off-by: dgzlopes <danielgonzalezlopes@gmail.com>

--- python_liftbridge/base.py
+++ python_liftbridge/base.py
@@ -24,15 +24,16 @@ class BaseClient(object):
                 'Creating a secure channel with address: %s' %
                 self.ip_address,
             )
-            self.stub = self._secure_connect(tls_cert)
+            self.channel = self._secure_channel(self.ip_address,tls_cert)
+            self.stub = self._connect()
         else:
             logger.debug(
                 'Creating an insecure channel with address: %s' % self.ip_address,
             )
             self.channel = grpc.insecure_channel(self.ip_address)
-            self.stub = self._insecure_connect()
+            self.stub = self._connect()
 
-    def _insecure_connect(self):
+    def _connect(self):
         try:
             grpc.channel_ready_future(
                 self.channel,
@@ -42,9 +43,12 @@ class BaseClient(object):
         else:
             return python_liftbridge.api_pb2_grpc.APIStub(self.channel)
 
-    def _secure_connect(self, secure_file):
-        # TODO
-        pass
+    def _secure_channel(self, address, secure_file):
+        with open(secure_file) as f:
+            trusted_certs = f.read().encode()
+        credentials = grpc.ssl_channel_credentials(root_certificates=trusted_certs)
+        return grpc.secure_channel(address, credentials)
+        
 
     def close(self):
         logger.debug('Closing channel')
